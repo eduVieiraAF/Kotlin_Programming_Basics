1ade4720f8958c1b725ef92b44f5e46014931a37
eduVieiraAF <edu_vieira@hotmail.com>
2022-01-06 11:59:31 -0300

    Basic functions and extension
    the ABC of coding with functions

---
+++ src/OOP/Functions.kt
@@ -0,0 +1,32 @@
+package OOP
+
+fun main(){
+
+    //print10Numbers()
+
+    val x = isEven(7)
+    val y = 5
+
+    println(x)
+    println(y.isOdd())
+}
+
+fun print10Numbers(){
+
+    for (i in 1..10){
+
+        println("The number is $i.")
+    }
+}
+
+fun isEven(number: Int = 4): Boolean {
+
+    return number % 2 == 0
+}
+
+//extension function
+
+fun Int.isOdd(): Boolean{
+
+    return this % 2 == 1
+}
