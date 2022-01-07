6dbfd40fcbbd827a6292ac1a2e9941cd27a74c75
eduVieiraAF <edu_vieira@hotmail.com>
2022-01-07 11:23:18 -0300

    Lambda
    Just little bit on the topic

---
+++ src/OOP/Lambda.kt
@@ -0,0 +1,27 @@
+package OOP
+
+fun main(){
+
+    val list = listOf("Kotlin", "Java", "JavaScript")
+    val count = list.customCount { currentSting ->
+
+        currentSting.length > 4
+    }
+
+    println(count)
+}
+
+// fun <T> List<T>.customCount(function: (T) -> Boolean): Int{    *in case you wanna use this in any type of list
+fun List<String>.customCount(function: (String) -> Boolean): Int{
+
+    var counter = 0
+    for(string in this){
+
+        if(function(string)) {
+
+            counter++
+        }
+    }
+
+    return counter
+}
