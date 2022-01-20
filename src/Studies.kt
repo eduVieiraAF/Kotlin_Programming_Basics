864f66ae75815a13198c65cbbe1097240d9a6a90
eduVieiraAF <edu_vieira@hotmail.com>
2022-01-20 10:37:03 -0300

    Trimmin' it

--- src/Studies.kt
+++ src/Studies.kt
@@ -5,15 +5,15 @@ fun main() {
 }
 fun reverse(list: List<Int>): List<Int>{
     val result = arrayListOf<Int>()
-    for (i in 0..list.size-1){
-        result.add(list.get(list.size-1-i))
+    for (i in list.indices){
+        result.add(list[list.size-1-i])
     }
     return result
 }
 fun reverse2 (list: List<Int>): List<Int>{
     val result = arrayListOf<Int>()
     for (i in list.size-1 downTo 0){
-        result.add(list.get(i))
+        result.add(list[i])
     }
     return result
 }
