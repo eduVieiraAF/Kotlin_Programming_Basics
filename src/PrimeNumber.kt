c4fc592a458127d519f9a921af980c41e4450d2c
eduVieiraAF <edu_vieira@hotmail.com>
2022-02-13 10:17:18 -0300

    Prime
    Little code to check whether a number is prime or not.

---
+++ src/PrimeNumber.kt
@@ -0,0 +1,18 @@
+
+fun main(){
+
+    val num = readln().toInt()
+    var flag = false
+
+    for (count in 2..num / 2){
+
+        if (num % count == 0){
+
+            flag = true
+            break
+        }
+    }
+
+    if (!flag) println("$num is a prime number")
+    else println("$num is not a prime number")
+}
