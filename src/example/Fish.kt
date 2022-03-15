0a082aa8422cdf1460b710f50c684d514065fe68
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-15 09:14:30 -0300

    High-order and more lambda

---
+++ src/example/Fish.kt
@@ -0,0 +1,33 @@
+package example
+
+data class Fish(var name: String)
+
+fun fishExamples(){
+
+    val fish = Fish("splashy")
+    myWith(fish.name){ println(capitalize()) }
+
+    fish.run { name }
+
+    val fish2 = Fish(name = "splashy").apply { name = "sharky" }
+
+    myWith(fish2.name, object : Function1<String, Unit>{
+
+        override fun invoke (name: String){ name.capitalize()}
+    })
+
+    println(fish2.name)
+
+    println(
+        fish.name.capitalize()
+        .let{it + "fish"}.length
+        .let{it + 31})
+    println(fish)
+}
+
+fun main(){
+
+    fishExamples()
+}
+
+fun myWith(name: String, block: String.() -> Unit){name.block()}
--- src/generics/MoreBasics.kt
+++ src/generics/MoreBasics.kt
@@ -1,11 +1,2 @@
 package samples
 
-class MyList<T>{
-
-    fun get(pos: Int): T {
-
-        TODO("implement")
-    }
-
-    fun addItem (item: T) {}
-}
