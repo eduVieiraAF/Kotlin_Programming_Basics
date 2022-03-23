c5e76845567036fd03577489529b80066ed729b9
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-23 15:31:10 -0300

    Control and fun
    Functions and control flow with when

--- src/aquarium/AMS.kt
+++ src/aquarium/AMS.kt
@@ -4,16 +4,42 @@ import kotlin.random.Random
 
 
 fun main(){
-
     feedTheFish()
 }
+
+fun getDirtySensorReading () = 20
+
+fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = getDirtySensorReading()): Boolean{
+
+        return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
+}
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
 
 fun feedTheFish(){
 
     val day = randomDay()
     val food = fishFood(day)
 
+    shouldChangeWater(day)
+    shouldChangeWater(day,20,50)
+    shouldChangeWater(day, dirty = 50)
+    shouldChangeWater("Monday")
+
+    if (shouldChangeWater(day)) println("Change the water today")
+
     println("Today is $day and the fish eat $food")
+
+    swim(50, "slow")
 }
 
 fun fishFood(day: String): String {
@@ -39,3 +65,5 @@ fun randomDay(): String {
 
     return week[Random.nextInt(7)]
 }
+
+fun swim(time: Int, speed: String = "fast"){}
