8d6f495890c7123e6392e9ba60babc4f1a96820b
eduVieiraAF <edu_vieira@hotmail.com>
2022-03-29 09:01:55 -0300

    Constructors and init

--- src/aquarium/Aquarium.kt
+++ src/aquarium/Aquarium.kt
@@ -1,11 +1,16 @@
 package aquarium
 
-class Aquarium {
+class Aquarium(var length: Int = 100,  var width: Int = 20, var height: Int = 40) {
 
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
     var volume: Int
         get() =  width * height * length / 1000
         set(value) { height = (value * 100) / (width * length)}
+    var water = volume * 0.9
+
+    constructor(numberOfFish: Int): this() {
+
+        val water = numberOfFish * 2000
+        val tank = water + water * 0.1
+        height =  (tank / (length * width)).toInt()
+    }
 }
--- src/aquarium/Main.kt
+++ src/aquarium/Main.kt
@@ -18,4 +18,15 @@ fun buildAquarium() {
     myAquarium.height = 80
     println("Height: ${myAquarium.height}")
     println(println("The volume is ${myAquarium.volume} litres"))
+
+    println()
+
+    val smallAquarium = Aquarium(20, 15, 30)
+    println("Small aquarium: ${smallAquarium.volume} litres")
+
+    val aquarium2 = Aquarium(numberOfFish = 9)
+
+    println()
+    println("Small aquarium 2: ${aquarium2.volume} litres with length ${aquarium2.length} " +
+            "width ${aquarium2.width} " + "height ${aquarium2.height}")
 }
