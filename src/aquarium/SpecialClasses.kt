99697915dc3481f2978293139244cecaff5d9223
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-02 13:28:27 -0300

    Special classes
    Class types and whatnot

---
+++ src/aquarium/SpecialClasses.kt
@@ -0,0 +1,35 @@
+package aquarium
+
+object MobyDickWhale{
+
+    val author = " Herman Melville"
+
+    fun jump() {
+
+        // TODO (code here)
+
+    }
+}
+
+enum class Color (val rgb: Int) {
+
+    RED (0xFF0000),
+    GREEN (0x00FF00),
+    BLUE (0x0000FF)
+
+}
+
+sealed class Seal
+
+class SeaLion: Seal()
+class Walrus: Seal()
+
+fun matchSeal(seal: Seal): String {
+
+    return when(seal){
+
+        is Walrus -> "walrus"
+        is SeaLion -> "Sea Lion"
+
+    }
+}
