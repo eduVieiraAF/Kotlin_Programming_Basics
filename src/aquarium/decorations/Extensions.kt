9a2212722c0472004dc8abe7613ad7d1bbc10b88
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-04 08:49:23 -0300

    Extensions
    Boost your code and make its functionalities even more geekily beautiful

---
+++ src/aquarium/decorations/Extensions.kt
@@ -0,0 +1,49 @@
+package aquarium.decorations
+
+/*fun String.hasSpaces(): Boolean{
+
+    val found: Char? = this.find { it == ' ' }
+    return found != null
+
+}*/
+
+fun String.hasSpaces() = find { it == ' ' } != null
+
+fun extensionsExample() = "Does it have spaces?".hasSpaces() // true
+
+open class AquariumPlant(val color: String, private val size: Int)
+class GreenLeafyPlant(size: Int): AquariumPlant("Green", size)
+
+fun AquariumPlant.isRed() = color == "Red"
+
+fun AquariumPlant.print() = println("AquariumPlant")
+fun GreenLeafyPlant.print() = println("GreenLeafyPlant")
+
+fun staticExample(){
+
+    val plant = GreenLeafyPlant(50)
+    plant.print()
+
+    val aquariumPlants: AquariumPlant = plant
+    aquariumPlants.print()
+
+}
+
+val AquariumPlant.isGreen: Boolean
+    get() = color == "Green"
+
+fun propertyExxample(){
+
+    val plant = AquariumPlant("Green", 50)
+    plant.isGreen // true
+
+}
+
+fun AquariumPlant?.pull() {this?.apply { println("Removing $this") }}
+
+fun nullableExample() {
+
+    val plant: AquariumPlant? = null
+    plant.pull()
+
+}
