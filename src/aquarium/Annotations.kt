66db0a3a113a0acc54f410f4812004039786f138
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-05 10:43:24 -0300

    labels

--- src/aquarium/Annotations.kt
+++ src/aquarium/Annotations.kt
@@ -8,14 +8,26 @@ import kotlin.reflect.full.findAnnotation
     fun trim(){}
     fun fertilize(){}
 
+    @get: OnGet
+    val isGrowing = true
+
+    @set: OnSet
+    var needsFood = false
+
 }
 
 annotation class ImPlant
 
+@Target(AnnotationTarget.PROPERTY_GETTER)
+annotation class OnGet
+
+@Target(AnnotationTarget.PROPERTY_SETTER)
+annotation class OnSet
+
 fun reflections(){
 
     val classObj = Plant::class
-//   for (method in classObj.declaredMemberFunctions){println(method.name)}
+    for (method in classObj.declaredMemberFunctions){println(method.name)}
 
     for (method in classObj.annotations) {println(method.annotationClass.simpleName)}
 
---
+++ src/aquarium/Labels.kt
@@ -0,0 +1,13 @@
+package aquarium
+
+fun labels(){
+
+    loop@ for (i in 1..100){
+
+        for (j in 1..100){
+
+            if (i > 10) break@loop
+
+        }
+    }
+}
