4a8208698b5bbb82cec520d1bbd947aca0ece1e1
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-06 20:58:12 -0300

    Let
    One more example on Let

---
+++ src/samples/Let.kt
@@ -0,0 +1,50 @@
+/* let
+
+The Kotlin standard library function let can be used for scoping and null-checks. When called on an object,
+let executes the given block of code and returns the result of its last expression. The object is accessible inside
+the block by the reference it (by default) or a custom name.
+ */
+
+fun customPrint(s: String) {
+
+    print(s.uppercase())
+}
+
+fun main() {
+
+    val empty = "test".let {               // Calls the given block on the result on the string "test".
+
+        customPrint(it)                    // Calls the function on "test" by the 'it' reference.
+        it.isEmpty()                       // let returns the value of this expression.
+    }
+
+    println(" is empty: $empty")
+
+    fun printNonNull(str: String?) {
+
+        println("Printing \"$str\":")
+
+        str?.let {                           // Uses safe call, so let and its code block will be executed
+                                             // only on non-null values.
+            print("\t")
+            customPrint(it)
+            println()
+        }
+    }
+
+    fun printIfBothNonNull(strOne: String?, strTwo: String?) {
+
+        strOne?.let { firstString ->         // Uses the custom name instead of it, so that the nested let can
+                                             // access the context object of the outer let.
+            strTwo?.let { secondString ->
+
+                customPrint("$firstString : $secondString")
+                println()
+            }
+        }
+    }
+
+    printNonNull(null)
+    printNonNull("my string")
+    printIfBothNonNull("First","Second")
+}
