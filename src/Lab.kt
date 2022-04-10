ac8c71ade9bee70991b2bbf499d9878ebedb8c24
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-10 10:03:12 -0300

    Reverse a number
    Little logic to reverse a number

--- src/Lab.kt
+++ src/Lab.kt
@@ -1,4 +1,16 @@
 
 fun main() {
 
+    var input = readln().toInt()
+    var reversedNum = 0
+
+    while (input != 0) {
+
+        val digit = input % 10
+        reversedNum = reversedNum * 10 + digit
+        input /= 10
+    }
+
+    println("Reversed Number: $reversedNum")
 }
+
