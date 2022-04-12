294a6bbafda9770e196d75a796029522f7faef7d
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-11 21:05:44 -0300

    Counting input
    Simple logic to count how many vowels, consonants, digits nad spaces in an input

---
+++ src/samples/CountingInput.kt
@@ -0,0 +1,27 @@
+package samples
+
+fun main() {
+
+    var line = readln()
+    var vowels = 0
+    var consonants = 0
+    var digits = 0
+    var spaces = 0
+
+    line = line.lowercase()
+
+    for (element in line){
+
+        when (element) {
+            'a', 'e', 'i', 'o', 'u' -> ++vowels
+            in 'a'..'z' -> ++consonants
+            in '0'..'9' -> ++digits
+            ' ' -> ++spaces
+        }
+    }
+
+    println("$vowels vowel(s)")
+    println("$consonants consonant(s)")
+    println("$digits digit(s)")
+    println("$spaces space(s)")
+}
