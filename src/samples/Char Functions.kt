7e5266d69a3a70cc3251d472e249b5e7e42f5f1e
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-12 15:58:40 -0300

    Char Functions (fix) Few useful functions to use with Char

--- src/samples/Char Functions.kt
+++ src/samples/Char Functions.kt
@@ -1,2 +1,18 @@
 package samples
 
+fun main() {
+
+    val char = 'a'
+
+    println(char.isDigit())
+    println(char.isLetter())
+    println(char.isLetterOrDigit())
+    println(char.isWhitespace())
+    println(char.isLowerCase())
+    println(char.isUpperCase())
+    println(char.uppercaseChar())
+    println(char.lowercaseChar())
+    println(char.uppercase())
+    println(char.lowercase())
+
+}
