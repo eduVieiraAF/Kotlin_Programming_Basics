073cf8a6e8d2b740bc58b831a34a2d4ae146a792
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-14 19:58:04 -0300

    Prime number
    Fixes

--- src/samples/PrimeNumber.kt
+++ src/samples/PrimeNumber.kt
@@ -1,18 +1,15 @@
 
 fun main(){
-
-    /*val num = readln().toInt()
+    val num = readln().toInt()
     var flag = false
 
     for (count in 2..num / 2){
-
         if (num % count == 0){
-
             flag = true
             break
         }
     }
 
     if (!flag) println("$num is a prime number")
-    else println("$num is not a prime number")*/
+    else println("$num is not a prime number")
 }
