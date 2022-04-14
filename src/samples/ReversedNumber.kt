22045c18c1abe6d3e1f90574d5e408d7b2813bdb
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-14 19:56:03 -0300

    Reverse Numbers
    Reverse order of numbers in an array (fix)

--- src/samples/ReversedNumber.kt
+++ src/samples/ReversedNumber.kt
@@ -2,23 +2,10 @@ package samples
 
 fun main() {
 
-    var input = readln().toInt()
-    var reversedNum = 0
-
-    while (input != 0) {
-
-        val digit = input % 10
-        reversedNum = reversedNum * 10 + digit
-        input /= 10
-    }
-
-    println("Reversed Number: $reversedNum")
+    val number = readln().toCharArray()
+    println(number.reversedArray())
 }
 
 
-    /*
-    val number = readln().toCharArray()
-    println(number.reversedArray())
 
-    This is another good option
-     */
+
