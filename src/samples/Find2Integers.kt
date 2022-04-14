fd891fea6858730c3df8a39e8f6e698fd8808ec1
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-14 07:17:46 -0300

    find 2 integers
    find if 2 elements occur in a mutable list

---
+++ src/samples/Find2Integers.kt
@@ -0,0 +1,43 @@
+package samples
+
+
+/*
+The first line contains the size of the list.
+The next N lines contain the elements of the list.
+The last line contains two integer numbers P and M separated by a space.
+
+If both numbers occur in the list you need to print YES, otherwise – NO.
+ */
+
+fun main() {
+
+    val listSize = readln().toInt()
+    val myList = mutableListOf<Int>()
+    repeat(listSize) { myList.add(readln().toInt()) }
+    val (p, m) = readln().split(" ")
+    var flag = false
+
+    for (i in listSize - 1 downTo 0) {
+        if (p.toInt() == myList[i]) {
+            for (j in listSize - 1 downTo 0) {
+                if (m.toInt() == myList[j]) flag = true
+            }
+        }
+    }
+    when (flag) {
+        true -> println("YES")
+        false -> println("NO")
+    }
+}
+
+/*
+
+Here's another alternative but no nested for loops as th challenge suggested
+
+fun main() {
+    val listSize = readln().toInt()
+    val mylist = MutableList(listSize) { readln().toInt() }
+    val s = readln().split(" ").map(String::toInt)
+    println(if (mylist.containsAll(s)) "YES" else "NO")
+}
+*/
