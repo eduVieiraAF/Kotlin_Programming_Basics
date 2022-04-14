11a86cfabcac178fc0f68bfca45bc11ca5da45f1
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-14 19:44:16 -0300

    Extension function
    Minor improvements

--- src/samples/Extension function.kt
+++ src/samples/Extension function.kt
@@ -6,10 +6,11 @@ fun String.removeFirstLastChar(): String =  this.substring(1, this.length - 1)
 
 fun main() {
 
-    val myString= "Hello World"
+    print("Type in anything: ")
+    val myString = readln()
     val result = myString.removeFirstLastChar()
 
-    println("First character is: $result")
+    println("String has been trimmed =  $result")
 }
 
 
