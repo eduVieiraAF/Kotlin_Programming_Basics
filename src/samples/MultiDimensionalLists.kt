528c081092590ac464b59133be3ff63d5acc90fb
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-18 11:39:25 -0300

    Multidimensional list pt.Deux
    Useful functions and a 3D list.

--- src/samples/MultiDimensionalLists.kt
+++ src/samples/MultiDimensionalLists.kt
@@ -23,11 +23,17 @@ fun main(){
         mutableListOf("not", "to", "be")
     )
 
+    println()
+    println(mutListOfString2D[0]) // prints out as a list
+    println(mutListOfString2D[1].joinToString()) // prints out as a "String"
+
     // Multidimensional list of Type Char
     val mutListOfChar2D = mutableListOf(
         mutableListOf('A', 'R', 'R'),
         mutableListOf('A', 'Y', 'S')
     )
+    println()
+    println(mutListOfChar2D[0].joinToString())
 
     // Multidimensional list of various Types
     val mutListOfVariousTypes2D = mutableListOf(
@@ -38,4 +44,16 @@ fun main(){
 
     println()
     println(mutListOfVariousTypes2D[2])
+
+    val mutList3D = mutableListOf(
+        mutableListOf(mutableListOf(0,1), mutableListOf(2,3)),
+        mutableListOf(mutableListOf(4,5), mutableListOf(6,7))
+    )
+
+    /*The following code creates the three-dimensional mutable list, here's a representation
+         [0, 1]	[2, 3]
+         [4, 5]	[6, 7]
+    */
+    println()
+    println(mutList3D) // prints out like -> [[[0, 1], [2, 3]], [[4, 5], [6, 7]]]
 }
