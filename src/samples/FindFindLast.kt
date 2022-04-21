a7ef198eb8163acc184b4841740a8258dde1b35c
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-21 10:44:48 -0300

    Find, findLast
    addendum

--- src/samples/FindFindLast.kt
+++ src/samples/FindFindLast.kt
@@ -7,9 +7,9 @@ fun main() {
     val words = listOf("Lets", "find", "something", "in", "collection", "somehow")
     val first = words.find { it.startsWith("some") } // looks for 1st word starting with "some"
     val last = words.findLast { it.endsWith("ow") } // looks for last word ending with "ow"
-    val nothing = words.find { it.contains("nothing") }
+    val thing = words.find { it.contains("thing") }
 
     println("The first word starting with \"some\" is \"$first\"")
     println("The last word ending with \"ow\" is \"$last\"")
-    println("The first word containing \"nothing\" is ${nothing?.let { "\"$it\"" } ?: "null"}")
+    println("The first word containing \"thing\" is ${thing?.let { "\"$it\"" } ?: "null"}")
 }
