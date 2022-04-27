dfb7affb95414ea0f576de191bad313a457198ae
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-27 18:48:25 -0300

    Delegation Properties

---
+++ src/samples/DelegatedProperties.kt
@@ -0,0 +1,32 @@
+package samples
+
+/*
+→ Kotlin provides a mechanism of delegated properties that allows delegating the calls of the property set and
+get methods to a certain object. The delegate object in this case should have the method getValue. For mutable
+properties, you'll also need setValue.
+*/
+
+import kotlin.reflect.KProperty
+
+class Example {
+    var OOP: String by Delegate()
+
+    override fun toString(): String = "Example Class"
+}
+
+class Delegate() {
+    operator fun getValue(thisRef: Any?, prop: KProperty<*>): String {
+        return "$thisRef, thank you fo delegating '${prop.name}' to me."
+    }
+
+    operator fun setValue(thisRef: Any?, prop: KProperty<*>, value: String) {
+        println("$value has been assigned to ${prop.name} in $thisRef.")
+    }
+}
+
+fun main() {
+    val example = Example()
+
+    println(example.OOP)
+    example.OOP = "\"New assignment\""
+}
