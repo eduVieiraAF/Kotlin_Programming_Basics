6d393ce3dc44fecda4712947c2c33259fffb103f
eduVieiraAF <edu_vieira@hotmail.com>
2022-04-29 19:36:36 -0300

    Named arguments
    Possible ways to use and pass arguments.

---
+++ src/samples/NamedArguments.kt
@@ -0,0 +1,14 @@
+package samples
+
+fun format(userName: String, domain: String) = "$userName@$domain"
+
+fun main() {
+    // Regular function call
+    println(format("mario", "example.com"))
+    // Calls a function with switched arguments. No syntax errors, but the result domain.com@username is incorrect.
+    println(format("domain.com", "username"))
+    // Calls a function with named arguments.
+    println(format(userName = "foo", domain = "bar.com"))
+    // When invoking a function with named arguments, you can specify them in any order you like.
+    println(format(domain = "frog.com", userName = "pepe"))
+}
