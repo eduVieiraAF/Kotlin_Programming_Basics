3fe7f5cb8013e9510b23ee055f78f7e7abe9c285
eduVieiraAF <edu_vieira@hotmail.com>
2022-05-31 08:33:19 -0300

    Adding enum

--- src/oop2/EntityFactory.kt
+++ src/oop2/EntityFactory.kt
@@ -1,7 +1,25 @@
 package oop2
 
+import java.util.*
+
+enum class EntityTypes {
+    EASY, MEDIUM, HARD;
+
+    fun getFormattedName() = name.lowercase()
+        .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+}
+
 object EntityFactory {
-    fun create() = Entity("id", "name")
+    fun create(type: EntityTypes) : Entity {
+        val id = UUID.randomUUID().toString()
+        val name = when (type){
+            EntityTypes.EASY -> type.getFormattedName()
+            EntityTypes.MEDIUM -> type.getFormattedName()
+            EntityTypes.HARD -> type.getFormattedName()
+        }
+
+        return Entity(id, name)
+    }
 }
 class Entity constructor(val id: String, val name: String) {
     override fun toString(): String {
@@ -11,6 +29,9 @@ class Entity constructor(val id: String, val name: String) {
 
 fun main() {
 //companion objects have access to properties & methods of that enclosing class
-    val entity = EntityFactory.create()
+    val entity = EntityFactory.create(EntityTypes.EASY)
     println(entity)
+
+    val mediumEntity = EntityFactory.create(EntityTypes.MEDIUM)
+    println(mediumEntity)
 }
