67264941f1bc9585bd5783b48cd5161427fb7e64
eduVieiraAF <edu_vieira@hotmail.com>
2022-06-03 10:23:21 -0300

    Additions

--- src/housing_oop/House.kt
+++ src/housing_oop/House.kt
@@ -1,19 +1,15 @@
 package housing_oop
 
 class House(rooms: Int, color: String) {
-    var color: String
-    val rooms: Int
+    private var color: String
+    private val rooms: Int
 
     init {
         println("Property details\n" +
                 "••••••••••••••••••••••••••••••••••••••••••••••••••••")
-    }
-    init {
         this.color = color
         println("Color → ${this.color.capitalized()}")
-    }
 
-    init {
         this.rooms = rooms
         println("Rooms → ${this.rooms}")
     }
@@ -23,11 +19,17 @@ class House(rooms: Int, color: String) {
     }
 
     constructor(rooms: Int, bathrooms: Double, color: String, hoaFee: Double): this(rooms, color) {
-        println("Bathrooms → $bathrooms\n" +
+            println("Bathrooms → $bathrooms\n" +
                 "Fee → $hoaFee")
     }
 
-    fun hasPool(pool: Boolean) {
-        if (pool) println("Pool included") else println("No pool")
+    constructor(rooms: Int, bathrooms: Double, color: String, hoaFee: Double, storeys: Int): this(rooms, color) {
+        println("Bathrooms → $bathrooms\n" +
+                "Fee → $hoaFee\n" +
+                "$storeys-storey house")
     }
+
+    fun hasPool(pool: Boolean) = if (pool) println("Pool included") else println("No pool")
+
+    fun hasGarage(spots: Int) = println("Parking space for $spots cars.")
 }
--- src/housing_oop/Main.kt
+++ src/housing_oop/Main.kt
@@ -1,7 +1,19 @@
 package housing_oop
 
 fun main() {
-    val house = House(5, 2.5,"red", 500.00)
+    val house = House(3, 2.0,"red", 500.00)
     house.hasPool(true)
+
+    println()
+
+    val house2 = House(5, 3.0, "white")
+    house2.hasPool(true)
+    house2.hasGarage(2)
+
+    println()
+
+    val house3 = House(5,4.0,"blue", 650.0, 2)
+    house3.hasPool(false)
+    house3.hasGarage(3)
 }
 
