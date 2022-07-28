a67bd0977fdd12d614373c27579d46779b840de1
Eduardo Vieira <edu_vieira@hotmail.com>
2022-07-27 21:44:55 -0300

    Nested Try Block

---
+++ src/samples/NestedTryBlock.kt
@@ -0,0 +1,20 @@
+package samples
+
+import housing_oop.prettyPrintln
+
+fun main() {
+    val num = intArrayOf(4, 8, 16, 32, 64, 128)
+    val deno = intArrayOf(2, 0, 4, 4, 0, 8)
+
+    try {
+        for (i in num.indices) {
+            try {
+                prettyPrintln(num[i].toString() + "/" + deno[i] + " is " + num[i] / deno[i])
+            } catch (e: ArithmeticException) {
+                prettyPrintln("Cannot be divided")
+            }
+        }
+    } catch (e: ArrayIndexOutOfBoundsException) {
+        prettyPrintln("Not found")
+    }
+}
