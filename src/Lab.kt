ed5bb8d49e1b38b70ffd826e1bd58cef8c74f255
Eduardo Vieira <edu_vieira@hotmail.com>
2022-07-27 22:07:57 -0300

    Trying out extensions

---
+++ src/Lab.kt
@@ -0,0 +1,12 @@
+import housing_oop.invert
+import housing_oop.prettyPrintln
+
+fun main() {
+    val int = 5
+    val float = 4.8
+    val double = -7.9
+
+    prettyPrintln(int.invert())
+    prettyPrintln(float.invert())
+    prettyPrintln(double.invert())
+}
