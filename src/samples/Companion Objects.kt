5e38892c120a10092fa69a33db3b1c004197e097
Eduardo Vieira <edu_vieira@hotmail.com>
2022-08-09 20:15:24 -0300

    Adjustments

--- src/samples/Companion Objects.kt
+++ src/samples/Companion Objects.kt
@@ -1,15 +1,21 @@
 package samples
 
-class Person {
+import housing_oop.prettyPrintln
 
+class Person {
     // name of companion object can be omitted
     companion object /*Test*/ {
+        fun callMe() = prettyPrintln("I'm called.")
+        fun callMeName(name: String) = prettyPrintln("I'm $name, good to meet you.")
+        fun callMeInfo(
+            name: String, age: Int
+        ) = prettyPrintln("I'm $name → $age years old. Tell about yourself...")
 
-        fun callMe() = println("I'm called.")
     }
 }
 
-fun main(){
-
+fun main() {
     Person.callMe()
+    Person.callMeName("Mitch")
+    Person.callMeInfo("Taylor", 40)
 }
--- src/samples/Count.kt
+++ src/samples/Count.kt
@@ -1,12 +1,14 @@
 package samples
 
+import housing_oop.prettyPrintln
+
 fun main() {
     val numbers = listOf(1, -2, 3, -4, 5, -6)
     val totalCount = numbers.count()
     val evenCount = numbers.count { it % 2 == 0 }
     val positiveCount = numbers.count { it > 0 }
 
-    println("Total number of elements: $totalCount")
-    println("Number of even elements: $evenCount")
-    println("Number of positive elements: $positiveCount")
+    prettyPrintln("Total number of elements: $totalCount")
+    prettyPrintln("Number of even elements: $evenCount")
+    prettyPrintln("Number of positive elements: $positiveCount")
 }
