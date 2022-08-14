dfedc331b6a991de505700bda4cb20def77bc966
eduVieiraAF <edu_vieira@hotmail.com>
2022-08-14 11:36:23 -0300

    Helper class LoginHttpEndpointSyncTd

--- src/udemy/exercise4/LoginUseCaseSyncTest.kt
+++ src/udemy/exercise4/LoginUseCaseSyncTest.kt
@@ -1,9 +1,10 @@
 package udemy.exercise4
 
-import org.junit.Assert.*
+import udemy.exercise4.authtoken.AuthTokenCache
 import udemy.exercise4.eventbus.EventBusPoster
 import udemy.exercise4.eventbus.LoggedInEvent
 import udemy.exercise4.networking.LoginHttpEndpointSync
+import udemy.exercise4.networking.NetworkErrorExceptions
 
 class LoginUseCaseSyncTest {
     companion object {
@@ -12,11 +13,37 @@ class LoginUseCaseSyncTest {
         const val AUTH_TOKEN = "authToken"
     }
 
-// *••••••••••••••••HELPER•CLASSES••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
 
-    class LoginHttpEndpointSyncTd: LoginHttpEndpointSync {
+
+//*••••••••••••••••HELPER•CLASSES••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+
+    class LoginHttpEndpointSyncTd(
+            private var mUsername: String,
+            private var mPassword: String,
+            private val mIsGeneralError: Boolean,
+            private val mIsAuthError: Boolean,
+            private val mIsServerError: Boolean,
+            private val mIsNetworkError: Boolean
+            ) : LoginHttpEndpointSync {
         override fun loginSync(username: String?, password: String?): LoginHttpEndpointSync.EndpointResult? {
-            TODO("Not yet implemented")
+            mUsername = username!!
+            mPassword = password!!
+
+            return if (mIsGeneralError) LoginHttpEndpointSync.EndpointResult(
+                    LoginHttpEndpointSync.EndpointResultStatus.GENERAL_ERROR,
+                    ""
+            )
+            else if (mIsAuthError) LoginHttpEndpointSync.EndpointResult(
+                    LoginHttpEndpointSync.EndpointResultStatus.AUTH_ERROR,
+                    ""
+            ) else if (mIsServerError) LoginHttpEndpointSync.EndpointResult(
+                    LoginHttpEndpointSync.EndpointResultStatus.SERVER_ERROR,
+                    ""
+            ) else if (mIsNetworkError) throw NetworkErrorExceptions()
+            else LoginHttpEndpointSync.EndpointResult(
+                    LoginHttpEndpointSync.EndpointResultStatus.SUCCESS,
+                    AUTH_TOKEN
+            )
         }
     }
 
@@ -24,6 +51,15 @@ class LoginUseCaseSyncTest {
         override fun postEvent(event: LoggedInEvent) {
             TODO("Not yet implemented")
         }
+    }
 
+    class AuthTokenCacheTd: AuthTokenCache {
+        override fun cacheAuthToken(authToken: String) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getAuthToken() {
+            TODO("Not yet implemented")
+        }
     }
 }
