07cf57333aad239fcc9a736327634f6d3ac6934a
Eduardo Vieira <edu_vieira@hotmail.com>
2022-12-14 20:31:44 -0300

    playing around

--- src/Lab.kt
+++ src/Lab.kt
@@ -1,6 +1,20 @@
-import samples.decimalPlaces
+import jdk.internal.org.jline.utils.InputStreamReader
+import sun.net.www.protocol.http.HttpURLConnection
+import java.io.BufferedReader
+
+import java.net.URL
 
 fun main() {
-    val result = 4.8 * Math.PI
-    println("Decimal point in place → ${result.decimalPlaces(2)}")
+    val url = URL("https://api.coindesk.com/v1/bpi/currentprice.json")
+
+    val connection = url.openConnection() as HttpURLConnection
+    connection.requestMethod = "GET"
+    connection.connect()
+
+    val responseCode = connection.responseCode
+    val responseBody =
+        BufferedReader(InputStreamReader(connection.inputStream)).use { it.readText() }
+
+    println("Status code:  $responseCode")
+    println("Status body:  $responseBody")
 }
--- src/samples/Exceptions.kt
+++ src/samples/Exceptions.kt
@@ -1,11 +1,10 @@
-package OOP
+package samples
 
-fun main(){
-    val number = readLine()?: "0"
+fun main() {
+    val number = readlnOrNull() ?: "0"
     val parsedNumber = try { // tries to do what your code was designed for
         number.toInt()
-    }
-    catch (e: Exception) { // in case it fails, it will return sth else
+    } catch (e: Exception) { // in case it fails, it will return sth else
         "Not a number. Nice try bucket-head."
     }
     println(parsedNumber)
