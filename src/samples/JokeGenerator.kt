52e2c06ed1722e06ae3407fb470bf180bc0b4f34
Eduardo Vieira <edu_vieira@hotmail.com>
2022-12-17 10:37:17 -0300

    Jokes generator

---
+++ src/samples/JokeGenerator.kt
@@ -0,0 +1,35 @@
+package samples
+
+import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+fun main() {
+    val jokes = mutableListOf<Joke>()
+
+    for (i in 1..5) {
+        val joke = getJoke()
+        jokes.add(joke)
+    }
+
+    jokes.forEach { println("${it.id}\n${it.type}\n\t${it.setup}\n${it.punchline}") }
+}
+
+class Joke(val id: Int, val type: String, val setup: String, val punchline: String)
+
+fun getJoke(): Joke {
+    val endpoint = "https://official-joke-api.appspot.com/random_joke"
+    val url = URL(endpoint)
+
+    val conn = url.openConnection() as HttpURLConnection
+    conn.requestMethod = "GET"
+
+    val reader = BufferedReader(InputStreamReader(conn.inputStream))
+    val response = reader.readText()
+
+    val gson = Gson()
+
+    return gson.fromJson(response, Joke::class.java)
+}
