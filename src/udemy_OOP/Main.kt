cb78e74ac0eaae79a32df401bd3d3316daa988f4
Eduardo Vieira <edu_vieira@hotmail.com>
2023-01-03 20:53:36 -0300

    Lesson #5 data class

--- src/udemy_OOP/Main.kt
+++ src/udemy_OOP/Main.kt
@@ -23,4 +23,24 @@ fun main() {
 
     // myCar.myModel = "M3" cannot be reassigned due to 'private' keyword but its getter works
     prettyPrintln(myCar.myModel)
+
+    prettyPrintln()
+
+    val user1 = User(1, "Eduardo")
+    println(user1.name)
+
+    val name = user1.name
+    val id = user1.id
+    println(id)
+
+    val user2 = User(1, "Heather")
+    println(user1.equals(user2)) // alternatively "==" instead of equals
+
+    // data class also makes available toString and copy
+    val user3 = user2.copy(name = "Joyce")
+    println(user3)
+    println(user3.component1()) // the id parameter
+    println(user3.component2()) // the name parameter
+
+    val (code, avatar) = user1
 }
---
+++ src/udemy_OOP/User.kt
@@ -0,0 +1,5 @@
+package udemy_OOP
+
+data class User(val id: Long, var name: String)
+// * A data class must hold at least ONE parameter
+
